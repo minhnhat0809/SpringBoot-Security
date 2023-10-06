ba30ae8b249cf6bd203478f4ad3190251e3971f6
minhnhat0809 <137554321+minhnhat0809@users.noreply.github.com>
2023-10-06 14:00:12 +0700

    SpringBoot_Security_v1

---
+++ src/main/java/com/example/demo/controller/AccountController.java
@@ -0,0 +1,26 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Accounts;
+import com.example.demo.repository.AccountsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AccountController {
+
+    @Autowired
+    private AccountsRepository accountsRepository;
+
+    @GetMapping("/myAccount")
+    public Accounts getAccountDetails(@RequestParam int id) {
+        Accounts accounts = accountsRepository.findByCustomerId(id);
+        if (accounts != null ) {
+            return accounts;
+        }else {
+            return null;
+        }
+    }
+
+}
---
+++ src/main/java/com/example/demo/repository/LoanRepository.java
@@ -0,0 +1,17 @@
+package com.example.demo.repository;
+
+import java.util.List;
+
+import com.example.demo.model.Loans;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface LoanRepository extends CrudRepository<Loans, Long> {
+
+    // @PreAuthorize("hasRole('USER')")
+    List<Loans> findByCustomerIdOrderByStartDtDesc(int customerId);
+
+}
